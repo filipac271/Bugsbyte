453429f44a76798ddf8c8ab49074443fac1daa92
a107329 <a107329@alunos.uminho.pt>
2025-03-29 22:19:23 +0000

    Alteracao no main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,7 +9,7 @@ df = pd.read_csv("dados.csv")
 @app.get('/search')
 def search(q: str = Query(..., min_length=1)):
     # Filtrar produtos pelo nome
-    resultados = df[df["Produtos"].str.contains(q, case=False, na=False)]
+    resultados = df[df["Produtos"].str.match(rf"^{q}s?\b", case=False, na=False)]
 
     resultados = resultados.fillna("")  # Substitui NaN por strings vazias
 
