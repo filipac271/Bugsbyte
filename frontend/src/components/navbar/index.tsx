d690c6e0b9dd256d741bdf0a42748bc46206aa2d
a107329 <a107329@alunos.uminho.pt>
2025-03-30 12:50:47 +0100

    Add botao dos mais vendidos

--- frontend/src/components/navbar/index.tsx
+++ frontend/src/components/navbar/index.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import DarkModeToggle from "../DarkModeToggle";
+import { useState, useEffect } from "react";
+import TopProdutos from "./TopProdutos";
 
 type Props = {};
 
 const Navbar = (props: Props) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
   const flexBetween = "flex items-center justify-between ";
   return (
     <nav>
@@ -25,13 +29,23 @@ const Navbar = (props: Props) => {
           </div>
           {/**Lada direito o tal do botao */}
           <div className={`${flexBetween} `}>
-            {/** o tal botao*/}
-            <div>
+            <div className="flex items-center gap-x-4">
+              <button
+                onClick={() => setIsOpen(true)}
+                className="h-[50px] p-2 px-3 py-2 rounded-full hover:bg-green-500 font-semibold bg-primary-100 dark:bg-slate-800 text-gray-800 dark:text-gray-200"
+              >
+                Mais vendidos ✨
+              </button>
               <DarkModeToggle />
             </div>
           </div>
         </div>
       </div>
+      {isOpen && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+          <TopProdutos setIsOpen={setIsOpen} />
+        </div>
+      )}
     </nav>
   );
 };
